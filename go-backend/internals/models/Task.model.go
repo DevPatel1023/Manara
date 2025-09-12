80706988f1de6b4936e806d37ec3b0f13cba9125
DevPatel1023 <devp4143@gmail.com>
2025-09-12 22:09:41 +0530

    feat : added new model task

---
+++ go-backend/internals/models/Task.model.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type TaskStatus string
+
+const (
+	Todo TaskStatus = "TODO",
+	InProgress TaskStatus = "INPROGRESS",
+	Done TaskStatus = "DONE",
+)
+
+type Task struct {
+	gorm.Model
+
+	ProjectID uint
+	Project Project `gorm:"foreignKey:ProjectId"`
+
+	AssignedToEMPID uint
+	AssignedTo User `gorm:"foreignKey:UserId"`
+
+	Title string
+	Description string
+	Deadline time.Time
+	Status TaskStatus
+}
