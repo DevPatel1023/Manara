6b449a73094c5a888e57a6cd9fb20c3d56666297
DevPatel1023 <devp4143@gmail.com>
2025-10-09 21:53:46 +0530

    feat : rfq db func's

--- go-backend/internals/repository/RFQ.impl.go
+++ go-backend/internals/repository/RFQ.impl.go
@@ -18,3 +18,21 @@ func (r *RFQImplementation) GetAllRFQS() ([]models.RFQ, error) {
 	err := r.DB.Preload("client").Find(&rfqs).Error
 	return rfqs, err
 }
+
+func (r *RFQImplementation) GetRFQById(id uint) (*models.RFQ, error) {
+	var rfq models.RFQ
+	err := r.DB.Preload("client").Find(&rfq, id).Error
+	return &rfq, err
+}
+
+func (r *RFQImplementation) UpdateRFQFields(rfq *models.RFQ) error {
+	return r.DB.Where("id = ?", rfq.ID).Save(rfq).Error
+}
+
+func (r *RFQImplementation) ReviewdRFQ(id uint, status models.RFQStatus) error {
+	return r.DB.Model(&models.RFQ{}).Where("id = ?", id).Update("Status", status).Error
+}
+
+func (r *RFQImplementation) DeleteRFQ(id uint) error {
+	return r.DB.Model(&models.RFQ{}).Delete("id = ?", id).Error
+}
--- go-backend/internals/repository/RFQ.repo.go
+++ go-backend/internals/repository/RFQ.repo.go
@@ -5,5 +5,8 @@ import "github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/models
 type RFQRepository interface {
 	CreateNewRFQ(model *models.RFQ) error
 	GetAllRFQS() ([]models.RFQ, error)
+	GetRFQById(id uint) (*models.RFQ, error)
+	UpdateRFQFields(model *models.RFQ) error
 	ReviewdRFQ(model *models.RFQ) error
+	DeleteRFQ(id uint) error
 }
