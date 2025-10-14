96d2b4313052fa30d723e69d61a1a87a94149fcc
DevPatel1023 <devp4143@gmail.com>
2025-10-14 11:31:28 +0530

    feat : implementing quotation routes

---
+++ go-backend/routes/quotation.routes.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/controllers"
+	"github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterQuoteRoutes(router *gin.RouterGroup, quoteController *controllers.QuoteController) {
+	quote := router.Group("/quotes")
+	quote.Use(middlewares.AuthMiddleware())
+
+	// Employee: Create & Update before admin approval
+	emp := quote.Group("")
+	emp.Use(middlewares.AuthorizeRole("employee"))
+	{
+		emp.POST("/", quoteController.CreateNewQuotation)  // default status: draft
+		emp.PATCH("/:id", quoteController.UpdateQuotation) // editable while draft
+	}
+
+	// Admin: Review & Send to Client
+	admin := quote.Group("")
+	admin.Use(middlewares.AuthorizeRole("admin"))
+	{
+		admin.GET("/", quoteController.GetAllQuotations)
+		admin.PATCH("/:id/status", quoteController.UpdateQuotationStatus) // draft -> sent
+	}
+
+	// Client: Accept / Reject Quotation
+	client := quote.Group("")
+	client.Use(middlewares.AuthorizeRole("client"))
+	{
+		client.PATCH("/:id/status", quoteController.UpdateQuotationStatus) // sent -> accepted/rejected
+	}
+
+	// Admin & Employee: View quotation details
+	view := quote.Group("")
+	view.Use(middlewares.AuthorizeRole("admin", "employee"))
+	{
+		view.GET("/:id", quoteController.GetQuotationByID)
+	}
+}
