cf110eed7007182d84ae02e04686a6ef950bd561
DevPatel1023 <devp4143@gmail.com>
2025-10-22 16:30:55 +0530

    feaat : implemented invoice method structure

---
+++ go-backend/internals/repository/Invoice.repo.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/models"
+)
+
+type InvoiceRepository interface {
+	CreateNewInvoice(i *models.Invoice) error
+	GetInvoiceByID(id uint) (*models.Invoice, error)
+	GetAllInvoices() ([]models.Invoice, error)
+	UpdateInvoice(i *models.Invoice) error
+	DeleteInvoice(id uint) error
+	GetInvoicesByCustomerID(customerID uint) ([]models.Invoice, error)
+	GetInvoicesByStatus(status string) ([]models.Invoice, error)
+	GetInvoicesByDateRange(startDate, endDate time.Time) ([]models.Invoice, error)
+	CountInvoices() (int64, error)
+}
