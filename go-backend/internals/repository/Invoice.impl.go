6111d64eb23863d67b3595da5d43c01ff0e736e4
DevPatel1023 <devp4143@gmail.com>
2025-10-24 10:34:56 +0530

    feat: added invoices db implementations and changes in repo

---
+++ go-backend/internals/repository/Invoice.impl.go
@@ -0,0 +1,62 @@
+package repository
+
+import (
+	"github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/models"
+	"gorm.io/gorm"
+)
+
+type InvoiceImplementation struct {
+	DB *gorm.DB
+}
+
+func (r *InvoiceImplementation) CreateNewInvoice(p *models.Invoice) error {
+	return r.DB.Create(p).Error
+}
+
+func (r *InvoiceImplementation) GetInvoiceByID(id uint) (*models.Invoice, error) {
+	var invoice models.Invoice
+	err := r.DB.Preload("Client").First(&invoice, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &invoice, err
+}
+
+func (r *InvoiceImplementation) GetAllInvoices() ([]models.Invoice, error) {
+	var invoices []models.Invoice
+	err := r.DB.Preload("Client").Find(&invoices).Error
+	if err != nil {
+		return nil, err
+	}
+	return invoices, err
+}
+
+func (r *InvoiceImplementation) UpdateInvoice(i *models.Invoice, id uint) (*models.Invoice, error) {
+	err := r.DB.Model(&models.Invoice{}).Where("id = ?", id).Updates(i).Error
+	if err != nil {
+		return nil, err
+	}
+	return i, err
+}
+
+func (r *InvoiceImplementation) DeleteInvoice(id uint) error {
+	return r.DB.Delete(&models.Invoice{}, id).Error
+}
+
+func (r *InvoiceImplementation) GetInvoicesByCustomerID(id uint) ([]models.Invoice, error) {
+	var invoices []models.Invoice
+	err := r.DB.Preload("Client").Where("ClientID = ?", id).Find(&invoices).Error
+	if err != nil {
+		return nil, err
+	}
+	return invoices, nil
+}
+
+func (r *InvoiceImplementation) GetInvoicesByStatus(status string) ([]models.Invoice, error) {
+	var invoices []models.Invoice
+	err := r.DB.Where("InvoiceStatus = ?", status).Find(&invoices).Error
+	if err != nil {
+		return nil, err
+	}
+	return invoices, nil
+}
--- go-backend/internals/repository/Invoice.repo.go
+++ go-backend/internals/repository/Invoice.repo.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"time"
-
 	"github.com/DevPatel1023/Quotation-to-invoice/go-backend/internals/models"
 )
 
@@ -10,10 +8,8 @@ type InvoiceRepository interface {
 	CreateNewInvoice(i *models.Invoice) error
 	GetInvoiceByID(id uint) (*models.Invoice, error)
 	GetAllInvoices() ([]models.Invoice, error)
-	UpdateInvoice(i *models.Invoice) error
+	UpdateInvoice(i *models.Invoice, id uint) (*models.Invoice, error)
 	DeleteInvoice(id uint) error
 	GetInvoicesByCustomerID(customerID uint) ([]models.Invoice, error)
 	GetInvoicesByStatus(status string) ([]models.Invoice, error)
-	GetInvoicesByDateRange(startDate, endDate time.Time) ([]models.Invoice, error)
-	CountInvoices() (int64, error)
 }
