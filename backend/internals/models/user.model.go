1837b663d6ca3a06000bd99a96adef996ca82358
Dev Patel <devp4143@gmail.com>
2026-04-13 23:08:11 +0530

    change: user model changes

--- backend/internals/models/user.model.go
+++ backend/internals/models/user.model.go
@@ -17,14 +17,12 @@ const (
 )
 
 type User struct {
-	gorm.Model
-
-	ID            uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	ID            uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Name          string    `json:"name" gorm:"not null"`
 	Email         string    `json:"email" gorm:"unique;not null"`
 	Password      string    `json:"-" gorm:"not null"`
 	Role          Role      `json:"role" gorm:"type:varchar(20);default:'Client'"`
-	CompanyName   uuid.UUID `json:"company_name" gorm:"not null;default:'Unknown'"`
+	CompanyID     uuid.UUID `json:"company_id" gorm:"not null;foreignKey"`
 	ContactNumber int       `json:"contact_number" gorm:"not null;default:0"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
